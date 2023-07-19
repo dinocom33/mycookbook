2db6e6d08e802a5e5fb45b317c0d993bad600530
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-07-19 22:09:29 +0300

    Change recipe details template

--- mycookbook/settings.py
+++ mycookbook/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
