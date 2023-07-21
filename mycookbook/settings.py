18651a4327dda2fee62bd607e99cbe8ba20c679d
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-07-21 16:16:58 +0300

    some changes

--- mycookbook/settings.py
+++ mycookbook/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static_files'
+STATIC_ROOT = BASE_DIR / 'static_files'
 STATICFILES_DIRS = [
     BASE_DIR / "static/",
 ]
