a4c795b665664ea578d84c302df96a33d7a93307
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-07-27 15:33:20 +0300

    Added change and reset password

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -6,7 +6,6 @@ from .views import edit_profile, ChangePasswordView, ResetPasswordView, ResetPas
 urlpatterns = [
     path('edit/<int:pk>/', edit_profile, name='edit profile'),
     path('password-change/', ChangePasswordView.as_view(), name='password change'),
-    path('password-change-done/', PasswordChangeDoneView.as_view(), name='password change done'),
     path('password-reset/', ResetPasswordView.as_view(), name='password reset'),
     path('password-reset-confirm/<uidb64>/<token>/', ResetPasswordConfirm.as_view(), name='password_reset_confirm'),
     path('password_reset_complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
